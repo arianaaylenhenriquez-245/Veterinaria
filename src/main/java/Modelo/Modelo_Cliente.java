900bff633c59ab14ce5b59eae19ad9d14cee1a3a
ariana <arianaaylenhenriquez@gmail.com>
2026-07-22 22:06:44 -0300

    Paquete MVC, con dos modelos

---
+++ src/main/java/Modelo/Modelo_Cliente.java
@@ -0,0 +1,7 @@
+package Modelo;
+
+public class Modelo_Cliente {
+    private int DNIcliente;
+    private String nombreCliente;
+    private String apellidoCliente;
+}
---
+++ src/main/java/com/mycompany/veterinaria/Veterinaria.java
@@ -0,0 +1,8 @@
+package com.mycompany.veterinaria;
+
+public class Veterinaria {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
